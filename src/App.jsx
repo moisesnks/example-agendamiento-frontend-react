f35e12a8dc29e5445b03abd6151fa8a302ce0809
moisesnks <mleiva@utem.cl>
2023-11-05 16:01:22 -0300

    Update App styling and add form and table components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './App.css';
 
 const App = () => {
   const [formData, setFormData] = useState({
@@ -35,23 +36,72 @@ const App = () => {
     }
   };
 
+  const renderForm = () => (
+    <form onSubmit={handleSubmit}>
+      <input type="number" name="origen_id" value={formData.origen_id} onChange={handleChange} />
+      <input type="number" name="destino_id" value={formData.destino_id} onChange={handleChange} />
+      <input type="date" name="fechaInit" value={formData.fechaInit} onChange={handleChange} />
+      <input type="date" name="fechaFin" value={formData.fechaFin} onChange={handleChange} />
+      <input type="number" name="personas" value={formData.personas} onChange={handleChange} />
+      <button type="submit">Enviar</button>
+    </form>
+  );
+
+  const renderTable = () => {
+    if (!response) {
+      return null;
+    }
+
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nombre</th>
+            <th>Total Personas</th>
+            <th>Fecha Inicio</th>
+            <th>Fecha Fin</th>
+            <th>Ciudad Origen</th>
+            <th>Ciudad Destino</th>
+            <th>Descripción</th>
+            <th>Detalles</th>
+            {/* Add other headers if needed, but omit row_number */}
+          </tr>
+        </thead>
+        <tbody>
+          {response.map((item, index) => (
+            <tr key={index}>
+              <td>{item.id}</td>
+              <td>{item.nombre}</td>
+              <td>{item.total_personas}</td>
+              <td>{item.fechainit}</td>
+              <td>{item.fechafin}</td>
+              <td>{item.nombre_ciudad_origen}</td>
+              <td>{item.nombre_ciudad_destino}</td>
+              <td>{item.descripcion}</td>
+              <td>{item.detalles}</td>
+              {/* Map other data fields as needed, but omit row_number */}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  };
+
   return (
     <div className="App">
       <h1>Envío de paquetes</h1>
-      <form onSubmit={handleSubmit}>
-        <input type="number" name="origen_id" value={formData.origen_id} onChange={handleChange} />
-        <input type="number" name="destino_id" value={formData.destino_id} onChange={handleChange} />
-        <input type="date" name="fechaInit" value={formData.fechaInit} onChange={handleChange} />
-        <input type="date" name="fechaFin" value={formData.fechaFin} onChange={handleChange} />
-        <input type="number" name="personas" value={formData.personas} onChange={handleChange} />
-        <button type="submit">Enviar</button>
-      </form>
-      <div>
-        <h3>Respuesta:</h3>
-        <pre>{JSON.stringify(response, null, 2)}</pre>
+      {renderForm()}
+      <div className="response-container">
+        <div className="json-response">
+          <h3>Respuesta:</h3>
+          <pre>{JSON.stringify(response, null, 2)}</pre>
+        </div>
+        {renderTable()}
       </div>
     </div>
   );
+
 };
 
 export default App;
